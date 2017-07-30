7e9361add17d587be6fcf05eb171257cf3b3ca89
gowatana <allezwat@gmail.com>
2017-07-30 19:53:49 +0900

    initial commit.

---
+++ create_NtnxCmdlets_shortcut.ps1
@@ -0,0 +1,20 @@
+# デスクトップに Nutainx Cmdlets のショートカットを作成する。
+
+$tool_name = "NtnxCmdlets"
+$profile_script_name = "set_ntnx_prompt.ps1"
+
+$shortcut_dir = Join-Path $HOME "Desktop"
+$shortcut_path = Join-Path $shortcut_dir ($tool_name + ".lnk")
+
+$tool_work_dir = (ls $PSCommandPath).DirectoryName
+$profile_path = $tool_work_dir $profile_script_name
+
+$ps = "powershell"
+$ps_argument = ("-NoExit", "-File",  $profile_path) -join " "
+
+$wsh = New-Object -ComObject WScript.Shell
+$shortcut = $wsh.CreateShortcut($shortcut_path)
+$shortcut.TargetPath = $ps
+$shortcut.Arguments = $ps_argument
+$shortcut.WorkingDirectory = (ls $profile_path).DirectoryName
+$shortcut.Save()
---
+++ set_ntnx_prompt.ps1
@@ -0,0 +1,34 @@
+﻿Add-PSSnapin NutanixCmdletsPSSnapin
+
+$window_width  = 150
+$window_height = 50
+$window_buffer = 3000
+
+$pshost = Get-Host 
+$pswindow = $pshost.ui.rawui
+
+$pswindow.WindowTitle = "Nutanix Cmdlets"
+
+$newsize = $pswindow.buffersize  
+$newsize.height = $window_buffer 
+$newsize.width = $window_width  
+$pswindow.buffersize = $newsize  
+  
+$newsize = $pswindow.windowsize  
+$newsize.height = $window_height  
+$newsize.width = $window_width  
+$pswindow.windowsize = $newsize
+
+Get-NTNXCmdletsInfo
+
+function prompt{
+    if((Get-NTNXConnectionInfo).Count -gt 0){
+        $prompt_color = "Green"
+    }else{
+        $prompt_color = "Gray"
+    }
+    
+    Write-Host "NTNX" -NoNewLine -ForegroundColor "Gray"
+    Write-Host ">" -NoNewLine -ForegroundColor $prompt_color
+    return " "
+}
